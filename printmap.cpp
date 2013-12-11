c260c5cfb9d235dde5343da03a98dfeaefc281fc
Ben Sechrist <ben@localhost.localdomain>
2013-12-10 19:44:32 -0500

    Initial map printer

---
+++ printmap.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+typedef enum colors { black = 30, red, green, yellow, blue, purple, aqua, white, grayBG = 40, redBG, greenBG, yellowBG, blueBG, purpleBG, aquaBG, whiteBG } consoleColors;
+
+int main(int argc, char ** argv) {
+	
+}
